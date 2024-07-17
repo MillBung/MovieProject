ec3e833666f6f1a5b0228d13eb60ae1198b2084a
qls0439 <qls0439@gmtc.kr>
2024-07-17 18:37:43 +0900

    Detail 작성 완료 및 css 수정 중

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,67 @@
-import {useParams, useState} from "react-router-dom";
-import {useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import styles from "./Detail.module.css";
+
 function Detail() {
     const {id} = useParams();
-    const [movie, setMovie] = useState("");
+    const [movie, setMovie] = useState(
+        {
+            id: 0,
+            title: "",
+            year: 0,
+            runtime: 0,
+            generes: [],
+            like_count: 0,
+            description_intro: "",
+            background_image: "",
+            medium_cover_image: ""
+        });
     const [loading, setLoading] = useState(true);
     const getMovie = async () => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
             ).json();
-        setMovie(json);
+        setMovie(
+            {
+                title: json.data.movie.title,
+                year: json.data.movie.year,
+                runtime: json.data.movie.runtime,
+                genres: json.data.movie.genres,
+                like_count: json.data.movie.like_count,
+                description_intro: json.data.movie.description_intro,
+                background_image: json.data.movie.background_image,
+                medium_cover_image: json.data.movie.large_cover_image
+            });
+        setLoading(false);
     };
     useEffect(() => {
         getMovie();
     }, []);
-    return <h1>Detail</h1>;
+    return (
+        <div className={styles.detailContainer}
+             style={{backgroundImage: `url(${movie.background_image}`}}>
+            {loading ? (
+                    <div className={styles.loader}>
+                        <span>Loading...</span>
+                    </div>
+                )
+                :
+                (
+                    <div className={styles.detailContent}>
+                        <img className={styles.coverImage} src={movie.medium_cover_image} alt={"Movie Cover"}/>
+                        <div className={styles.textContent}>
+                            <div>{movie.title} ({movie.year})</div>
+                            <div className={styles.detailGenres}>
+                                {movie.genres.map((g, index) => (
+                                    <div key={index} className={styles.genre}>{g}</div>
+                                ))}
+                            </div>
+                            <p className={styles.detailDescription}>{movie.description_intro.length > 1300 ? `${movie.description_intro.slice(0,1300)}...` : movie.description_intro}</p>
+                        </div>
+                    </div>
+                )}
+        </div>
+    );
 }
 
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
     const [loading, setLoading] = useState(true);
@@ -17,14 +18,18 @@ function Home() {
         getMovies();
     }, []);
     return (
-        <div>
-            {loading ? (<h1>Loading...</h1>
+        <div className={styles.container}>
+            {loading ? (
+                <div className={styles.loader}>
+                    <span>Loading...</span>
+                </div>
             ) : (
-                <div>
+                <div className={styles.movies}>
                     {movies.map(movie => (
                         <Movie
                             key={movie.id}
                             id={movie.id}
+                            year={movie.year}
                             coverImg={movie.medium_cover_image}
                             title={movie.title}
                             summary={movie.summary}
